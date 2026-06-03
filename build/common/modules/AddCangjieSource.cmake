8755ef6a54c869867fbcccb7c71ae13a079774f4
jinbohang <jinbohang1@huawei.com>
2026-06-03 11:57:54 +0800

    feat: support mac lto
    Signed-off-by: jinbohang <jinbohang1@huawei.com>

--- build/common/modules/AddCangjieSource.cmake
+++ build/common/modules/AddCangjieSource.cmake
@@ -389,6 +389,7 @@ function(add_cangjie_library target_name
     set(COMPILE_BC_CMD
         ${COMPILE_CMD}
         --lto=full
+        $<$<BOOL:${IOS}>:--experimental>
         ${output_argument}
         ${output_lto_bc_full_name})
     set(COMPILE_CMD ${COMPILE_CMD} ${output_argument} ${output_full_name})
@@ -470,18 +471,18 @@ function(add_cangjie_library target_name
     endif()
     if(CANGJIE_CODEGEN_CJNATIVE_BACKEND
        AND NOT WIN32
-       AND NOT DARWIN)
+       AND (NOT DARWIN OR IOS))
         if(CMAKE_BUILD_STAGE STREQUAL "postBuild")
-            add_custom_target(	 
-                ${target_name}_bc ALL	 
-                COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/${output_bc_dir}	 
-                COMMAND ${CMAKE_COMMAND} -E env "CANGJIE_PATH=${bc_cangjie_path}" "LIBRARY_PATH=${CMAKE_BINARY_DIR}/lib"	 
-                        ${COMPILE_BC_CMD}	 
-                BYPRODUCTS ${output_lto_bc_full_name}	 
-                # The ${target_name}_bc depends on ${target_name} so they will not run simultaneously. <target> and <target>_bc 
-                # compile the same package, which means they may write the same bc cache file. Running simultaneously	 
-                # may cause IO error on windows in some cases.	 
-                DEPENDS ${bc_depends}	 
+            add_custom_target(
+                ${target_name}_bc ALL
+                COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/${output_bc_dir}
+                COMMAND ${CMAKE_COMMAND} -E env "CANGJIE_PATH=${bc_cangjie_path}" "LIBRARY_PATH=${CMAKE_BINARY_DIR}/lib"
+                        ${COMPILE_BC_CMD}
+                BYPRODUCTS ${output_lto_bc_full_name}
+                # The ${target_name}_bc depends on ${target_name} so they will not run simultaneously. <target> and <target>_bc
+                # compile the same package, which means they may write the same bc cache file. Running simultaneously
+                # may cause IO error on windows in some cases.
+                DEPENDS ${bc_depends}
                 COMMENT "Generating ${target_name}_bc")
         else()
             add_custom_command(
@@ -559,7 +560,7 @@ function(add_cangjie_library target_name
 
     if(CANGJIE_CODEGEN_CJNATIVE_BACKEND
        AND NOT WIN32
-       AND NOT DARWIN)
+       AND (NOT DARWIN OR IOS))
         list(APPEND install_files ${output_lto_bc_full_name})
     endif()
     if(CANGJIE_CODEGEN_CJNATIVE_BACKEND)
